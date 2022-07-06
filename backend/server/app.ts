8beb4b1d3ab10b126ffef2ae11fba7433d0c3f0b
Tholen <vtholen@contractor.ea.com>
2022-07-06 17:26:24 +0200

    cleanup

--- backend/server/app.ts
+++ backend/server/app.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import express from 'express';
 import cors from 'cors';
 
@@ -10,8 +9,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-// app.use('/', session);
-// app.use('/', register);
 app.use('/', info);
 
 export default app;
--- backend/server/routes/info.ts
+++ backend/server/routes/info.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import { info } from '../controllers/infoController';
 
 const router = express.Router();
-
-// Login
 router.get('/', info);
 
 export default router;
