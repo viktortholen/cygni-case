8e1d2c35881e3915bf760e3d457d042c2f7aefcb
Tholen <vtholen@contractor.ea.com>
2022-07-06 17:08:17 +0200

    backend done, basic frontend done

--- backend/server/controllers/infoController.ts
+++ backend/server/controllers/infoController.ts
@@ -2,57 +2,70 @@ import { Request, Response } from 'express';
 const axios = require('axios');
 
 export const info = async (req: Request, res: Response) => {
-//get the MBID
+    const mbid = req.query.mbid;
+    if(mbid == undefined)
+        return res.status(400).send({error: `No mbid was not provided as a parameter. Please provide a valid mdib.`});
+    
+    try{
+        const mbData = await getMusicBrainzData(mbid);
+        const wikiTitle = await getWikiTitle(mbData);
+        const wikiDesc = await getWikipediaDesc(wikiTitle);
+        //Formatting
+        const albums : object = mbData['release-groups'].map((a : any) => {
+            return {
+                title : a.title,
+                id : a. id,
+                image : coverArt(a.id)
+            };
+        });
 
-//send the request to musicbrainz
-const music_res = await musicbrainz();
-//send request to WIKI
-const wiki_res = await wikidata(music_res);
-//send request to Cover art
+        return res.status(200).send({
+            mbid,
+            description: wikiDesc,
+            albums,
+        });
+    }catch(err)
+    {
+        return res.status(404).send({error : `The mbid was invalid or did not match any results.`});
+    }
 
-
-/*
-● En beskrivning av artisten som hämtas från Wikipedia. Wikipedia innehåller
-inte några MBID utan mappningen mellan MBID och Wikipedia-identifierare
-finns via MusicBrainz API (antingen en direktreferens eller via språkproxyn
-Wikidata).
-● En lista över alla album som artisten släppt och länkar till bilder för varje
-album. Listan över album finns i MusicBrainz men bilderna finns på Cover Art
-Archive.
-
-*/
-
-  return res.status(200).send({
-    status: true,
-    wiki_res,
-  });
 };
 
 
-const musicbrainz = async() => {
-  
-    const exampleMDIB = "5b11f4ce-a62d-471e-81fc-a69a8278c7da";
+const getMusicBrainzData = async(mbid : any) => {
     const inc = "?&inc=url-rels+release-groups&type=album";
-    const url = process.env.MUSICBRAINZ_URL + 'artist/' + exampleMDIB + inc;
+    const url = process.env.MUSICBRAINZ_URL + 'artist/' + mbid + inc;
     console.log(url)
     return await axios.get(url)
         .then((res: any) => {
             return res.data;
         });
-    ///artist/<MBID>?inc=<INC>
 }
-const wikidata = async(res : any) => {
+const getWikiTitle = async(res : any) => {
+
     const wiki_url = res.relations.filter((d : any)=>{
         return d.type==="wikidata";
     })[0]?.url?.resource;
-
+    
     const id = wiki_url.substring(wiki_url.lastIndexOf('/') + 1);
     const options = "&format=json&props=sitelinks";
-    const url = process.env.WIKI_URL + '?action=wbgetentities' + "&ids=" + id + options;
-
+    const url = `${process.env.WIKI_URL}?action=wbgetentities&ids=${id}${options}`;
     return await axios.get(url)
         .then((res: any) => {
-            console.log(res.data.entities[id].sitelinks)
-            return res.data.entities[id].sitelinks;
+            let title = res.data.entities[id].sitelinks.enwiki.title;
+            return title.replace(' ', '_');
         });
+}
+const getWikipediaDesc = async(title : string) => {
+
+    const options = `&format=json&prop=extracts&exintro=true&redirects=true&titles=${title}`;
+    const url = `${process.env.WIKIPEDIA_URL}?action=query${options}`;
+    return await axios.get(url)
+        .then((res: any) => {
+            let pages = res.data.query.pages;
+            return pages[Object.keys(pages)[0]].extract;
+        });
+}
+const coverArt = (mbid : any) => {
+    return `${process.env.COVER_ART_ARCHIVE_URL}/release-group/${mbid}/front`;
 }
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,94 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+<template>  
+  <div class="wrapper">
+    <form action="/action_page.php" @submit.prevent="onRequest">
+    <label for="mbid">Artist MBID: </label>
+    <input class="inputfield" v-model="mbid" type="text" id="mbid" name="mbid"><button type="submit">Get data</button><br><br>
+    
+    </form>
+    <div v-html="getDescription" class="content"></div>
+    <div v-if="!loading" class="container">
+    <h3>Albums:</h3>
+    <div class="albums">
+      <div class="album" v-for="(a, i) in getAlbums" :key="i">
+        <div>
+          <div class="title">
+            {{a.title}}
+          </div>
+          <img class="img" :src="a.image" :alt=i>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data(){
+    return {
+      mbid: '',
+      description: '',
+      albums: [],
+      loading: true,
+    }
+  },
+  computed:{
+    getDescription()
+    {
+      return this.description;
+    },
+    getAlbums()
+    {
+      console.log(this.albums)
+      return this.albums;
+    },
+    albumsSet()
+    {
+      return this.albums.size > 0;
+    }
+
+  },
+  methods: {
+    onRequest: async function(){
+      try {
+        const url = "http://localhost:4000/?mbid="+this.mbid;
+        await axios.get(url).then(d =>{
+          this.description = d.data.description;
+          this.albums = d.data.albums;
+          this.loading = false;
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    },
+}
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.wrapper{
+  height: auto;
+}
+.content{
+  height: auto;
+}
+.img{
+  width: 50px;
+  height: 50px;
+}
+.albums {
+  display: grid;
+  grid-template-columns: repeat(5, minmax(92px,1fr));
+  gap: 10px;
+}
+.inputfield{
+  width: 300px;
+  height: 30px;
+  border-radius: 5px;
+}
+button{
+  width: 70px;
+  height: 40px;
+}
+</style>
